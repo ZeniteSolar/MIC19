9d90383adebaf583ac563a1732df43a85fbd470b
alejopm03 <perdomomilar@gmail.com>
2019-07-29 15:01:41 -0300

    MIC19 can read IOs

---
+++ Firmware/src/conf.h
@@ -0,0 +1,167 @@
+/**
+ * @file conf.h
+ *
+ * @defgroup CONF Configurations 
+ *
+ * @brief General configuration of the system.
+ *
+ */
+
+#ifndef CONF_H
+//#define CONF_H
+
+#ifndef F_CPU
+#define F_CPU 16000000UL    //define a frequencia do microcontrolador - 16MHz
+#endif /* ifndef F_CPU */
+
+
+// CONFIGURACOES DE COMPILACAO
+#define DEBUG_ON
+#define VERBOSE_ON
+//#define VERBOSE_ON_CAN_APP
+#define VERBOSE_ON_MACHINE
+//#define VERBOSE_ON_ADC
+#define VERBOSE_ON_INIT
+#define VERBOSE_ON_ERROR
+#define VERBOSE_ON_RELAY
+
+
+// MODULES ACTIVATION
+#define USART_ON
+#define CAN_ON
+#define CAN_DEPENDENT
+#define ADC_ON
+#define MACHINE_ON
+#define LED_ON
+#define BUZZER_ON
+#define WATCHDOG_ON
+#define SLEEP_ON	
+
+//PINS UPDATE FILTER CONFIGURATION
+#define BOAT_ON_TO_UPDATE 10
+#define MOTOR_ON_TO_UPDATE 10
+#define DEAD_MEN_TO_UPDATE 10
+
+
+
+#ifdef ADC_ON
+// ADC CONFIGURATION
+// note that changing ADC_FREQUENCY may cause problems with avg_sum_samples
+#define ADC_FREQUENCY                       10000 // 20000
+#define ADC_TIMER_PRESCALER                 8
+#define ADC0_AVG                            adc.channel[ADC0].avg
+#define ADC0_ANGULAR_COEF                   10000 //(40000/((4/5)*1024))
+#define ADC0_LINEAR_COEF                    0
+#define ADC1_AVG                            adc.channel[ADC1].avg
+#define ADC1_ANGULAR_COEF                   10000 //(40000/((4/5)*1024))
+#define ADC1_LINEAR_COEF                    0
+#define ADC_AVG_SIZE_2                      7                  // in base 2
+#define ADC_AVG_SIZE_10                     128                // in base 10
+
+
+//#define FAKE_ADC_ON
+#ifdef FAKE_ADC_ON
+#define FAKE_ADC                            1
+#endif // FAKE_ADC_ON
+
+#endif //ADC_ON
+
+
+#ifdef MACHINE_ON
+// The machine frequency may not be superior of ADC_FREQUENCY/ADC_AVG_SIZE_10
+#define MACHINE_TIMER_FREQUENCY             120           //<! machine timer frequency in Hz
+#define MACHINE_TIMER_PRESCALER             1024          //<! machine timer prescaler
+#define MACHINE_CLK_DIVIDER_VALUE           ((uint64_t)(uint32_t)MACHINE_TIMER_FREQUENCY*(uint32_t)ADC_AVG_SIZE_10)/(ADC_FREQUENCY)           //<! machine_run clock divider
+#define MACHINE_FREQUENCY                   (MACHINE_TIMER_FREQUENCY)/(MACHINE_CLK_DIVIDER_VALUE)
+
+// SCALE TO CONVERT ADC DEFINITIONS
+#define VSCALE                              (uint16_t)1000
+
+#endif // MACHINE_ON
+
+// INPUT PINS DEFINITIONS
+/*EXAMPLE OF INPUT PIN DEFINITONS
+#define     CHARGERELAY_PORT        PORTC
+#define     CHARGERELAY_PIN         PINC
+#define     CHARGERELAY_DDR         DDRC
+
+#define     CHARGERELAY             PC2
+#define     set_chargerelay()       set_bit(CHARGERELAY_PORT, CHARGERELAY)
+#define     clr_chargerelay()       clr_bit(CHARGERELAY_PORT, CHARGERELAY)
+*/
+
+
+#define     PUMPS_SWITCHES_PORT     PORTD
+#define     PUMPS_SWITCHES_PIN      PIND
+#define     PUMPS_SWITCHES_DDR      DDRD
+#define     PUMP1_ON_SWITCH         PD2
+#define     PUMP2_ON_SWITCH         PD3
+#define     PUMP3_ON_SWITCH         PD4
+
+#define     DMS_PORT                PORTD
+#define     DMS_PIN                 PIND
+#define     DMS_DDR                 DDRD
+#define     DMS                     PD5
+
+#define     CTRL_SWITCHES_PORT      PORTC
+#define     CTRL_SWITCHES_PIN       PINC
+#define     CTRL_SWITCHES_DDR       DDRC
+#define     BOAT_ON_SWITCH          PC3
+#define     MOTOR_ON_SWITCH         PC4
+#define     MCC_ON_SWITCH           PC5
+
+#ifdef LED_ON
+#define     LED_PORT                PORTD
+#define     LED_PIN                 PIND
+#define     LED_DDR                 DDRD
+#define     LED1                    PD6
+#define     cpl_led(y)              cpl_bit(LED_PORT, y)
+#define     set_led(y)              set_bit(LED_PORT, y)
+#define     clr_led(y)              clr_bit(LED_PORT, y)
+#else
+#define     cpl_led()               
+#define     set_led()               
+#define     clr_led()               
+#endif // LED_ON
+
+#ifdef BUZZER_ON
+#define     BUZZER_PORT             PORTD
+#define     BUZZER_PIN              PIND
+#define     BUZZER_DDR              DDRD
+#define     BUZZER                  PD7
+#define     cpl_buzzer()            cpl_bit(BUZZER_PORT, BUZZER)
+#define     set_buzzer()            set_bit(BUZZER_PORT, BUZZER)
+#define     clr_buzzer()            clr_bit(BUZZER_PORT, BUZZER)
+#endif
+
+#ifdef CAN_ON
+#define SPI_ON
+#define CAN_APP_SEND_STATE_FREQ     40//36000     //<! state msg frequency in Hz
+#define CAN_APP_SEND_ADC_FREQ       4//6000      //<! adc msg frequency in Hz
+
+// CANBUS DEFINITONS
+// ----------------------------------------------------------------------------
+/* Global settings for building the can-lib and application program.
+ *
+ * The following two #defines must be set identically for the can-lib and
+ * your application program. They control the underlying CAN struct. If the
+ * settings disagree, the underlying CAN struct will be broken, with
+ * unpredictable results.
+ * If can.h detects that any of the #defines is not defined, it will set them
+ * to the default values shown here, so it is in your own interest to have a
+ * consistent setting. Ommiting the #defines in both can-lib and application
+ * program will apply the defaults in a consistent way too.
+ *
+ * Select if you want to use 29 bit identifiers.
+ */
+#define	SUPPORT_EXTENDED_CANID	0
+
+/* Select if you want to use timestamps.
+ * Timestamps are sourced from a register internal to the AT90CAN.
+ * Selecting them on any other controller will have no effect, they will
+ * be 0 all the time.
+ */
+#define	SUPPORT_TIMESTAMPS		0
+#endif // CAN_ON
+
+#endif /* ifndef CONF_H */
---
+++ Firmware/src/machine.h
@@ -0,0 +1,125 @@
+/**
+ * @file machine.h
+ *
+ * @defgroup MACHINE State Machine Module
+ *
+ * @brief Implements the main state machine of the system.
+ *
+ */
+
+#ifndef MACHINE_H
+#define MACHINE_H 
+
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include "conf.h"
+
+// Equations for mode 2 (CTC with TOP OCR2A)
+// Note the resolution. For example.. at 150hz, ICR1 = PWM_TOP = 159, so it
+#define MACHINE_TIMER_TOP ((F_CPU/(2*MACHINE_TIMER_PRESCALER))/(MACHINE_TIMER_FREQUENCY) -1)
+
+#ifdef ADC_ON
+#include "adc.h"
+#endif
+#ifdef USART_ON
+#include "usart.h"
+#endif
+#include "dbg_vrb.h"
+#ifdef CAN_ON
+#include "can.h"
+#include "can_app.h"
+extern const uint8_t can_filter[];
+#endif
+
+typedef enum state_machine{
+    STATE_INITIALIZING,
+    STATE_IDLE,
+    STATE_RUNNING,
+    STATE_ERROR,
+    STATE_RESET,
+} state_machine_t;
+
+typedef union system_flags{
+    struct{
+        uint8_t     boat_on                :1;
+        uint8_t     motor_on               :1;
+        uint8_t     MCC_on                 :1;
+        uint8_t     dead_men_switch        :1;
+        uint8_t     pump1_on               :1;
+        uint8_t     pump2_on               :1;
+        uint8_t     pump3_on               :1;
+    };
+    uint8_t   all__;
+} system_flags_t;
+
+typedef union error_flags{
+    struct{
+        uint8_t     no_canbus     :1;
+    };
+    uint8_t   all;
+}error_flags_t;
+
+typedef struct control
+{
+    uint16_t motor_PWM_target;
+    uint16_t motor_RAMP_target;
+    uint16_t MCC_POWER_target;
+}control_t;
+control_t control;
+
+// machine checks
+void check_buffers(void);
+void reset_measurements(void);
+
+// debug functions
+void print_configurations(void);
+void print_system_flags(void);
+void print_error_flags(void);
+
+// machine tasks
+void task_initializing(void);
+void task_idle(void);
+void task_running(void);
+void task_error(void);
+void task_reset(void);
+void task_waiting_reset(void);
+
+// the machine itself
+void set_machine_initial_state(void);
+void machine_init(void);
+void machine_run(void);
+void set_state_error(void);
+void set_state_initializing(void);
+void set_state_idle(void);
+void set_state_running(void);
+void set_state_reset(void);
+void set_state_waiting_reset(void);
+
+//input functions
+void read_switches(void);
+void read_potenciometers(void);
+
+// machine variables
+volatile state_machine_t state_machine;
+volatile system_flags_t system_flags;
+volatile error_flags_t error_flags;
+volatile uint16_t charge_count_error;
+volatile uint8_t relay_clk;
+volatile uint8_t first_boat_off;
+volatile uint8_t machine_clk;
+volatile uint8_t machine_clk_divider;
+volatile uint8_t total_errors;           // Contagem de ERROS
+volatile uint16_t charge_count_error;
+volatile uint8_t reset_clk;
+
+// other variables
+volatile uint8_t led_clk_div;
+
+// ISRs
+ISR(TIMER2_COMPA_vect);
+ISR(PCINT2_vect);
+
+#endif /* ifndef MACHINE_H */
