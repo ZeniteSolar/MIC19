ec2b4b4f2af6301c4a80d8de054e842e07184c1f
ayresgit <ayres808@gmail.com>
2022-08-16 17:19:46 -0300

    Added my port to up code

--- firmware/up.sh
+++ firmware/up.sh
@@ -3,5 +3,6 @@ make clean
 rm -rf bin obj
 make all
 #fuser -k /dev/ttyUSB0
-avrdude -c arduino -P /dev/ttyUSB0 -p m328p -D -U flash:w:bin/firmware.elf
+#avrdude -c arduino -P /dev/ttyUSB0 -p m328p -D -U flash:w:bin/firmware.elf
+avrdude -c arduino -P /dev/ttyS8 -p m328p -D -U flash:w:bin/firmware.elf
 figlet "MIC19"
