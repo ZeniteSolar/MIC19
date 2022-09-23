1eb9fbf9dd38914d6a482c4b6e80f958b4324592
ayresgit <ayres808@gmail.com>
2022-09-23 19:27:13 -0300

    Changed port

--- firmware/up.sh
+++ firmware/up.sh
@@ -5,4 +5,4 @@ make all
 #fuser -k /dev/ttyUSB0
 #avrdude -c arduino -P /dev/ttyUSB0 -p m328p -D -U flash:w:bin/firmware.elf
 avrdude -c arduino -P /dev/ttyS8 -p m328p -D -U flash:w:bin/firmware.elf
-figlet "MIC19"
+#figlet "MIC19"
