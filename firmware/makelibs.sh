f6c5461638d24f154568665706aa23c0d8a0ef64
Gustavo-V-F <gugvfranca@gmail.com>
2025-04-13 11:40:25 -0300

    (feat): Change CAN IDS header file to another branch

--- firmware/makelibs.sh
+++ firmware/makelibs.sh
@@ -2,4 +2,4 @@ cd lib/avr-can-lib/src/
 make clean
 make lib
 cd ../../../
-wget https://raw.githubusercontent.com/ZeniteSolar/CAN_IDS/new_ids/can_ids.h -O src/can_ids.h
+wget https://raw.githubusercontent.com/ZeniteSolar/CAN_IDS/refs/heads/MNA23/can_ids.h -O src/can_ids.h
