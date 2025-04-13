db1c76031c165807ebd770b848e783b67cf3b043
Gustavo-V-F <gugvfranca@gmail.com>
2025-04-13 11:35:15 -0300

    (feat): Update lib bit utils macros

--- firmware/lib/bit_utils.h
+++ firmware/lib/bit_utils.h
@@ -10,8 +10,8 @@
 #define _BIT_UTILS_H_
 
 // FROM AVR:  http://www.atmel.com/webdoc/avrlibcreferencemanual/ch20s22s02.html
- #define bit_is_set(sfr,bit)              (_SFR_BYTE(sfr) & _BV(bit)) 
- #define bit_is_clear(sfr,bit)            (!(_SFR_BYTE(sfr) & _BV(bit)))
+// #define bit_is_set(sfr,bit)              (_SFR_BYTE(sfr) & _BV(bit)) 
+// #define bit_is_clear(sfr,bit)            (!(_SFR_BYTE(sfr) & _BV(bit)))
 // #define loop_until_bit_is_clear(sfr,bit) do { } while (bit_is_set(sfr, bit))  
 // #define loop_until_bit_is_set(sfr,bit)   do { } while (bit_is_clear(sfr, bit)) 
 // #define _BV(bit)                         (1 << (bit))
@@ -46,5 +46,20 @@
  * @brief Returns the high byte from a word
  */
 #define HIGH(y)         ((y>>8)&0xFF)
+
+/**
+ *  Concatena dois bytes 
+ */
+#define HIGH_LOW(z, y, x)     (z = (y << 8) | (x));
+
+/**
+ *  Detecta borda de subida
+ */
+#define RISING_EDGE(_new, _old)      (((_new) ^ (_old)) & (_new))
+
+/**
+ *  Detecta borda de subida
+ */
+#define FALLING_EDGE(_new, _old)      (((_new) ^ (_old)) & (_old))
 
 #endif /* ifndef _BIT_UTILS_H_*/
