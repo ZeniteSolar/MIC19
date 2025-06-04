3547706ccd9e58a3f44812590e4e9e1b7e01e8c5
ayresgit <ayres808@gmail.com>
2025-06-04 19:42:18 -0300

    Start serial with stty after loading code

--- firmware/up.sh
+++ firmware/up.sh
@@ -1,8 +1,8 @@
+
 #cppcheck  --force --std=c99 --language=c src/
 make clean
 rm -rf bin obj
 make all
-#fuser -k /dev/ttyUSB0
-#avrdude -c arduino -P /dev/ttyUSB0 -p m328p -D -U flash:w:bin/firmware.elf
-avrdude -c arduino -P /dev/ttyS8 -p m328p -D -U flash:w:bin/firmware.elf
-#figlet "MIC19"
+#fuser -k /dev/ttyACM0
+avrdude -c arduino -P /dev/ttyACM0 -p m328p -D -U flash:w:bin/firmware.elf
+stty 57600 < /dev/ttyACM0 && cat /dev/ttyACM0  
